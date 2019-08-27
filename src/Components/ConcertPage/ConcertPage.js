748833cdfc933fb9ab8e668399cd6cd69613b740
Liat <liatcohen9@gmail.com>
2019-08-27 19:13:34 +0300

    get concert info from server and load in page

--- src/Components/ConcertPage/ConcertPage.js
+++ src/Components/ConcertPage/ConcertPage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
 
+@inject("ConcertStore")
+@observer
 class ConcertPage extends Component {
+
+   componentDidMount(){
+      this.props.ConcertStore.getConcert(this.props.match.params.id)
+   }
    render() {
+
       return (
          <div>
-            ConcertPage!  
-            id: {this.props.match.params.id}
+
         </div>)
    }
 }
