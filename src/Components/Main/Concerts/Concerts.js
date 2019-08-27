2fce980f2c3ba2b58a4946af0be284b5ce945a86
Ofer Gilboa <ofer1gilboa@gmail.com>
2019-08-27 11:46:06 +0300

    routes

--- src/Components/Main/Concerts/Concerts.js
+++ src/Components/Main/Concerts/Concerts.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 
 class Concerts extends Component {
    render() {
       return (
          <div>
-            Concerts
+            Concert details
+
+            <Link to="/concertpage" className="Concerts"> Concert </Link>
+
         </div>)
    }
 }
