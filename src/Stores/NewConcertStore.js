a659f10b89abff2ac13ab78fb9908ced12b9d864
Ofer Gilboa <ofer1gilboa@gmail.com>
2019-08-27 15:30:34 +0300

    new concert

--- src/Stores/NewConcertStore.js
+++ src/Stores/NewConcertStore.js
@@ -8,7 +8,7 @@ export class NewConcertStore {
          artist: "",
          date: Date,
          hour: Date,
-         country: "israel",
+         country: "Israel",
          city: "Tel aviv",
          venue: "",
          num_of_tickets: Number,
@@ -16,19 +16,31 @@ export class NewConcertStore {
          original_price: Number,
          additional_info: "",
       }
-
    }
-   @action saveConcert = () => {
+   @action saveConcert = (concertInfo) => {
+      let concert={
+         artist: concertInfo.artist,
+         date: concertInfo.date,
+         hour: concertInfo.hour,
+         city: concertInfo.city,
+         venue: concertInfo.venue,
+         num_of_tickets: concertInfo.num_of_tickets,
+         asked_price: concertInfo.asked_price,
+         original_price: concertInfo.original_price,
+         additional_info: concertInfo.additional_info,
+      }
       // post(‘/concerts’)
+      console.log(concert)
    }
 
-   @action hndleInput = () => {
-      // set
+   @action handleInput = (name, value) => {
+      this.newConcert[name] = value
    }
-
-
-
 }
+
+
+
+
 
 
 
