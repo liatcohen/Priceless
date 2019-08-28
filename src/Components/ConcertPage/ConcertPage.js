2b2ac476e9caa8f159f742cdd46c186c17805008
Hadar Alon <hadaralon3@gmail.com>
2019-08-28 15:43:56 +0300

    ksmk

--- src/Components/ConcertPage/ConcertPage.js
+++ src/Components/ConcertPage/ConcertPage.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
+import Modal from 'react-awesome-modal';
 
 import './ConcertPage.css'
 
 const moment = require('moment')
 
+@inject("UserStore")
 @inject("ConcertStore")
 @observer
+
 class ConcertPage extends Component {
+
+
+   //=======================POPUP
+   constructor(props) {
+      super(props);
+      this.state = {
+          visible : false
+      }
+  }
+
+  openModal() {
+      this.setState({
+          visible : true
+      });
+  }
+
+  closeModal() {
+      this.setState({
+          visible : false
+      });
+  }
+
+ //=======================POPUP
+
 
    componentDidMount() {
       this.props.ConcertStore.getConcert(this.props.match.params.id)
@@ -16,7 +43,29 @@ class ConcertPage extends Component {
 
       return (
          <div className="concertSection">
-            <div className="concertPhoto"><img src={this.props.ConcertStore.concert.img_url} alt="Smiley face" height="300" width="700"></img></div>
+
+            <section>
+
+                <Modal 
+                    visible={this.state.visible}
+                    width="400"
+                    height="300"
+                    effect="fadeInUp"
+                    onClickAway={() => this.closeModal()}
+                >
+                    <div>
+                        <h1>Seller Info</h1>
+                        <div className="sellerInfoPop">
+                        <p><strong>Name:</strong> {this.props.UserStore.user.name}</p>
+                        <p><strong>Make a call:</strong><a className="phonePop" href={"tel:" + this.props.UserStore.user.phone_number}  >{this.props.UserStore.user.phone_number}</a></p>
+                        <p><strong>Send Email:</strong> {this.props.UserStore.user.email}</p>
+                        </div>
+                        <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                        <div className="brk-btn" id="popButton" onClick={() => this.closeModal()}> Close </div>
+                    </div>
+                </Modal>
+            </section>
+            <div className="concertPhoto" style={{backgroundImage: "url(" + this.props.ConcertStore.concert.img_url + ")" , backgroundSize: "51.2vw 22vw",   marginRight: "14.5%"  }}></div>
             <div className="concertTitle">{this.props.ConcertStore.concert.artist}</div>
             <div className="ticketSection">
 
@@ -32,13 +81,12 @@ class ConcertPage extends Component {
                      <div>{this.props.ConcertStore.concert.city}, {this.props.ConcertStore.concert.country}</div>
                      <div className="price">Price: {this.props.ConcertStore.concert.asked_price} $</div>
                   </div>
-                  <div className="buttonBox"><span className="contactButton">Contact Seller</span></div>
+                  <div className="buttonBox"><span onClick={() => this.openModal()} className="contactButton">Contact Seller</span></div>
                </div>
                <div className="info">
-                  <div>About</div>
-                  <div>{this.props.ConcertStore.concert.num_of_tickets} Tickets Available</div>
-
-                  <div>{this.props.ConcertStore.concert.additional_info}</div>
+                  <div className="iconBar"><i class="fas fa-ticket-alt iconConcert"></i>{this.props.ConcertStore.concert.num_of_tickets} Tickets</div>
+                  <div className="iconBar"><i class="fas fa-check-circle iconConcert"></i> 44% Saving</div>
+                  <div className="iconBar"><i class="fas fa-info-circle iconConcert"></i>{this.props.ConcertStore.concert.additional_info} </div>
                </div>
             </div>
          </div>)
--- src/Stores/UserStore.js
+++ src/Stores/UserStore.js
@@ -1,36 +1,27 @@
 import { observable, computed, action } from 'mobx'
 import axios from 'axios'
-
-
 export class UserStore {
-   @observable userId
+   @observable user = {}
    @observable name
    @observable favorites
-
    constructor() {
       this.userId = "2"
-      // -- VALUES ('Liat Cohen', 'liatcohen9@gmail.com', 0504211600);
-
-      this.name = "Shoobert"
+      this.user = {
+         id: '2',
+         name: 'Liat Cohen',
+         email: 'liatcohen9@gmail.com',
+         phone_number: '050-4211600'
+      }
+      
    }
    @action get = (concertId) => {
       // get(‘/concert:/concertId’)
       // Return specific concert
    }
-   // @computed get userId() {
-   //     return this.userId
-   // }
-
-   // @computed get userName() {
-   //     return this.name
-   // }
-   //
-
+  
    @action addToFavorites = async (concertId) => {
       console.log(this.userId)
       console.log(concertId)
       axios.post(`http://localhost:5000/favorite/:${this.userId}/:${concertId}`)
    }
-
-
 }
