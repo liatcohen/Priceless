8be45b16bcd157060a103130b3bdf00cc5860142
Ofer Gilboa <ofer1gilboa@gmail.com>
2019-08-28 13:44:11 +0300

    dont use

---
+++ src/culcPrec.js
@@ -0,0 +1,11 @@
+//percentage
+
+// @observable percentage
+
+// constructor() {
+//    this.percentage = Number
+// }
+
+// @action culcPercentage = (oldPrice, newPrice) => {
+//    return oldPrice / 100 * newPrice
+// }
