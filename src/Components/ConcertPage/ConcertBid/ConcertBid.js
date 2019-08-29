2c7fcce40148037d75567c8b1c5cc5d1ffbc6529
Liat <liatcohen9@gmail.com>
2019-08-29 10:16:35 +0300

    fix bug in bid time

--- src/Components/ConcertPage/ConcertBid/ConcertBid.js
+++ src/Components/ConcertPage/ConcertBid/ConcertBid.js
@@ -20,32 +20,33 @@ class ConcertBid extends Component {
         this.props.ConcertStore.getConcert(this.props.clientId)
 
         this.countdown()
-        this.setState({timer: this.props.ConcertStore.concert.ends_at})
+        this.setState({ timer: this.props.ConcertStore.concert.ends_at })
     }
     countdown = (e) => {
         console.log("countdown")
         console.log('30-08-2019 10:30:00')
-      console.log("ends at: ")
-      console.log(this.props.ConcertStore.concert.ends_at)
+        console.log("ends at: ")
+        console.log(this.props.ConcertStore.concert.ends_at)
         // var $clock = $('#clock'),
-       let time = moment(this.props.ConcertStore.concert.ends_at).format('DD-MM-YYYY HH:mm:ss')
-       console.log("ends at2: ")
-       console.log(time)
-       console.log("time") 
+        let time = moment(this.props.ConcertStore.concert.ends_at).format('DD-MM-YYYY HH:mm:ss')
+        console.log("ends at2: ")
+        console.log(time)
+        console.log("time")
 
-       console.log(time) 
-    //    var eventTime = moment(time).unix(),
-       var eventTime = moment(time, 'DD-MM-YYYY HH:mm:ss').unix(),
+        console.log(time)
+        //    var eventTime = moment(time).unix(),
+        var eventTime = moment(time, 'DD-MM-YYYY HH:mm:ss').unix(),
 
-       
-        // var eventTime = moment(this.props.ConcertStore.concert.ends_at, 'DD-MM-YYYY HH:mm:ss').unix(),
 
-        // var eventTime = moment('30-08-2019 10:30:00', 'DD-MM-YYYY HH:mm:ss').unix(),
+            // var eventTime = moment(this.props.ConcertStore.concert.ends_at, 'DD-MM-YYYY HH:mm:ss').unix(),
+
+            // var eventTime = moment('30-08-2019 10:30:00', 'DD-MM-YYYY HH:mm:ss').unix(),
             currentTime = moment().unix(),
             diffTime = eventTime - currentTime,
             duration = moment.duration(diffTime * 1000, 'milliseconds'),
             interval = 1000;
-
+        console.log("currentTime")
+        console.log(moment())
         // if time to countdown
         if (diffTime > 0) {
             setInterval(() => {
@@ -54,11 +55,11 @@ class ConcertBid extends Component {
                     h = moment.duration(duration).hours(),
                     m = moment.duration(duration).minutes(),
                     s = moment.duration(duration).seconds();
-                    console.log("in interval:")
+                console.log("in interval:")
                 console.log(`${d}:${h}:${m}:${s}`)
                 this.setState({ timer: `${d}:${h}:${m}:${s}` })
             }, interval);
-        }else{
+        } else {
             console.log("TIME is PASSED!!!!")
         }
     };
@@ -71,9 +72,9 @@ class ConcertBid extends Component {
         this.props.ConcertStore.makeBid()
     }
 
-    
+
     render() {
-console.log("LIAT")
+        console.log("LIAT")
         console.log(this.props.ConcertStore.concert.ends_at)
         return (
             <div className="concert-bid">
