c5d94677aa1425960cf68a7e1691e89a2144b803
Ofer Gilboa <ofer1gilboa@gmail.com>
2019-09-02 11:28:45 +0300

    logo and navbar active

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link , NavLink} from 'react-router-dom'
-import image from '../../images/logo1.png'
+import image from '../../images/logo4.png'
 import './Navbar.css'
 
 class Navbar extends Component {
@@ -8,7 +8,7 @@ class Navbar extends Component {
       return (
          <div className="navbar">
             
-            <Link to="/" className="Main Logo" activeClassName="active"> <img src={image} height="25" /> </Link>
+            <Link to="/" className="Main Logo" activeClassName="active"> <img src={image} height="30" /> </Link>
             <NavLink exact to="/" className="Main" activeClassName="active"> Home </NavLink>
             <NavLink to="/newitem" className="NewItem" activeClassName="active"> Add Concert </NavLink>
             <NavLink to="/favorites" className="Favorites" activeClassName="active"> Favorites </NavLink>
